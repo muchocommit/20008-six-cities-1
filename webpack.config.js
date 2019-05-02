2f2c8f4c541f64984df93f7f1577ef3fd716245b
Vadim Korabelnik <vad@oblako9.ru>
2019-05-02 22:39:32 +0300

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require(`path`);
+
+
+module.exports = {
+
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    compress: false,
+    publicPath: `http://localhost:8085/`,
+    hot: true,
+    port: 8085
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`
+};
