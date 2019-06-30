a983ac493265fe1f8c49ca4139e0269eec24a6bf
Vadim Korabelnik <vad@oblako9.ru>
2019-06-30 23:58:42 +0300

    Push for auto merge

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 export default class Header extends PureComponent {
   constructor(props) {
